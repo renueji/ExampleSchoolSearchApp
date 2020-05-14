bf88ff5ca7854caaff16f9194b14099b77a71a0d
uejo <uejo@yamashitasanenobu-no-MacBook-Pro.local>
2020-05-14 11:01:59 +0900

    Initial Commit

---
+++ ExampleSchoolSearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleSchoolSearchApp
+//
+//  Created by uejo on 2020/05/14.
+//  Copyright © 2020 uejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ExampleSchoolSearchAppTests/ExampleSchoolSearchAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  ExampleSchoolSearchAppTests.swift
+//  ExampleSchoolSearchAppTests
+//
+//  Created by uejo on 2020/05/14.
+//  Copyright © 2020 uejo. All rights reserved.
+//
+
+import XCTest
+@testable import ExampleSchoolSearchApp
+
+class ExampleSchoolSearchAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
